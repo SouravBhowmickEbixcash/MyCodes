f3e187348b62300a06350bbcccb1c0a250404cee
Sourav Bhowmick <sourav.bhowmick@Indussoft.com>
2026-03-13 16:46:51 +0530

    spring security tutorial 2

---
+++ codding-shuttle/SecurityApplication/src/main/java/com/example/SecurityApp/SecurityApplication/entities/User.java
@@ -0,0 +1,53 @@
+package com.example.SecurityApp.SecurityApplication.entities;
+
+import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    /**
+     * Returns the authorities granted to the user. Cannot return <code>null</code>.
+     *
+     * @return the authorities, sorted by natural key (never <code>null</code>)
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    /**
+     * Returns the password used to authenticate the user. Can be null if the user has not
+     * specified a password (e.g. the user Passkeys instead).
+     *
+     * @return the password
+     */
+    @Override
+    public @Nullable String getPassword() {
+        return this.password;
+    }
+
+    /**
+     * Returns the username used to authenticate the user. Cannot return
+     * <code>null</code>.
+     *
+     * @return the username (never <code>null</code>)
+     */
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
---
+++ codding-shuttle/SecurityApplication/src/main/java/com/example/SecurityApp/SecurityApplication/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.SecurityApp.SecurityApplication.repositories;
+
+import com.example.SecurityApp.SecurityApplication.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
